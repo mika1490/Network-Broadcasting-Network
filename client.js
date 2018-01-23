d82ea5f933feea9392e42689c6d2ee4ee5c74418
Mika Dumont <mikakrantman@gmail.com>
2018-01-23 13:46:04 -1000

    client to client messaging working

---
+++ client.js
@@ -0,0 +1,19 @@
+const net = require('net');
+
+const client = net.connect(10337, '0.0.0.0', () => {
+  console.log('Connected'); //acknowledge socket connection
+  client.write('Hello, Server!') // send info to server
+
+process.stdin.pipe(client);
+
+  
+});
+
+client.on('data', (data) => {
+  //display info received from server
+  console.log('Received: ' + data); 
+});
+
+client.on('close', () => {
+  console.log('Connection Closed');
+});
--- server.js
+++ server.js
@@ -1,6 +1,42 @@
 const net = require('net');
 
-// http.createServer(function (req, res) {
-//   res.write('Hello World!'); //write a response to the client
-//   res.end(); //end the response
-// }).listen(8080);
+//creates an empty array to push the data into
+let connections = [];
+
+//connects server
+const server = net.createServer((socket) => {
+  // connections.write('hi');
+  connections.push(socket);
+
+
+  //socket is now able to retreive data
+  socket.on('data', (data) => {
+    //the data that goes through the socket will be encoded with utf8
+    socket.setEncoding('utf8');
+    console.log(data);
+    //makes sure that client doesnt receive their own message 
+    connections.filter(element => {
+      return element !== socket;
+      console.log(data);
+
+    })
+      .forEach(element => {
+        element.write(data)
+      });
+
+
+    socket.on('end', () => {
+      //splice makes sure that client doesnt receive their own message
+      connections.splice(connections.indexOf(socket, 1));
+      console.log('Client Disconnected')
+    });
+  });
+});
+
+//binds server to a port
+server.listen(10337, '0.0.0.0', () => {
+  console.log('You Are Now Connected');
+});
+
+
+
